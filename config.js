89e908b8b81276c456aa4fa9fce8a5d305dc2b0b
Stu Kennedy <info@continuata.com>
2016-07-05 00:40:20 +0100

    file-based config

--- config.js
+++ config.js
@@ -18,21 +18,9 @@ exports.FB_REQ   = process.env.FB_REQ || 'search/request';
 // The path in your Firebase where this app will write the results
 exports.FB_RES   = process.env.FB_RES || 'search/response';
 
-// See https://firebase.google.com/docs/web/setup#project_setup for how to
-// auto-generate this config
-exports.FB_SERVICEACCOUNT = {
-  "type": "service_account",
-  "project_id": process.env.FB_NAME || null,
-  "private_key_id": process.env.FB_PRIVATEKEY_ID || null,
-  "private_key": process.env.FB_PRIVATEKEY || null,
-  "client_email": process.env.FB_EMAIL || null,
-  "client_id": process.env.FB_CLIENT_ID,
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://accounts.google.com/o/oauth2/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/" + process.env.FB_EMAIL
-}
-
+// See https://firebase.google.com/docs/server/setup. for how to
+// auto-generate this config json ...
+exports.FB_SERVICEACCOUNT = process.env.FB_SERVICEJSONPATH
 
 /** ElasticSearch Settings
  *********************************************/
