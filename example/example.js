74c771ddab58c8917ddeec14b7fc2969f274663e
katowulf <katowulf@gmail.com>
2016-07-20 19:54:13 -0700

    Update example to 3.x SDK. Fixes #66.

--- example/example.js
+++ example/example.js
@@ -6,7 +6,28 @@
    /**====== SET ME =====**/
    /**====== SET ME =====**/
    /**====== SET ME =====**/
-   var URL = 'https://INSTANCE.firebaseio.com';
+   // Set the configuration for your app
+   // TODO: Replace with your project's config object
+   var config =  {
+      apiKey: "AIzaSyCQAwrCtKHfLmXU2q0lrk5rFzXSjcR5TIo",
+      authDomain: "flashlight.firebaseapp.com",
+      databaseURL: "https://flashlight.firebaseio.com",
+      storageBucket: "firebase-flashlight.appspot.com"
+   };
+
+  // TODO: Replace this with the path to your ElasticSearch queue
+  // TODO: This is monitored by your app.js node script on the server
+  // TODO: And this should match your seed/security_rules.json
+  var PATH = "search";
+  /**====== /SET ME =====**/
+  /**====== /SET ME =====**/
+  /**====== /SET ME =====**/
+
+  // Initialize connection using our project credentials
+  firebase.initializeApp(config);
+
+  // Get a reference to the database service
+   var database = firebase.database();
 
    // handle form submits
    $('form').on('submit', function(e) {
@@ -24,8 +45,8 @@
 
    // display search results
    function doSearch(index, type, query) {
-      var ref = new Firebase(URL+'/search');
-      var key = ref.child('request').push({ index: index, type: type, query: query }).key();
+      var ref = database.ref().child(PATH);
+      var key = ref.child('request').push({ index: index, type: type, query: query }).key;
       
       console.log('search', key, { index: index, type: type, query: query });
       ref.child('response/'+key).on('value', showResults);
@@ -34,8 +55,8 @@
    function showResults(snap) {
       if( !snap.exists() ) { return; } // wait until we get data
       var dat = snap.val();
-      snap.ref().off('value', showResults);
-      snap.ref().remove();
+      snap.ref.off('value', showResults);
+      snap.ref.remove();
       var $pair = $('#results')
          .text(JSON.stringify(dat, null, 2))
          .add( $('#total').text(dat.total) )
@@ -57,7 +78,7 @@
    }
 
    // display raw data for reference
-   new Firebase(URL).on('value', setRawData);
+   database.ref().on('value', setRawData);
    function setRawData(snap) {
       $('#raw').text(JSON.stringify(snap.val(), null, 2));
    }
