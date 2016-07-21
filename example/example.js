46fb4bb6b9bd9940f2cbb04993083be0c0d267fd
katowulf <katowulf@gmail.com>
2016-07-20 19:14:23 -0700

    Merge #54

--- example/example.js
+++ example/example.js
@@ -15,7 +15,7 @@
       var term = $form.find('[name="term"]').val();
       var words = $form.find('[name="words"]').is(':checked');
       if( term ) {
-         doSearch($form.find('[name="index"]').val(), $form.find('[name="type"]:checked').val(), buildQuery(term, words));
+         doSearch($form.find('[name="index"]').val(), $form.find('[name="type"]:checked').val(), makeTerm(term, words));
       }
       else {
          $('#results').text('');
@@ -26,6 +26,7 @@
    function doSearch(index, type, query) {
       var ref = new Firebase(URL+'/search');
       var key = ref.child('request').push({ index: index, type: type, query: query }).key();
+      
       console.log('search', key, { index: index, type: type, query: query });
       ref.child('response/'+key).on('value', showResults);
    }
@@ -33,7 +34,6 @@
    function showResults(snap) {
       if( snap.val() === null ) { return; } // wait until we get data
       var dat = snap.val();
-//      console.log('result', snap.name(), snap.val());
       snap.ref().off('value', showResults);
       snap.ref().remove();
       var $pair = $('#results')
@@ -46,14 +46,6 @@
       else if( dat.total < 1 ) {
          $pair.addClass('zero');
       }
-   }
-
-   function buildQuery(term, words) {
-      // See this tut for more query options:
-      // http://okfnlabs.org/blog/2013/07/01/elasticsearch-query-tutorial.html#match-all--find-everything
-      return {
-         'query_string': { query: makeTerm(term, words) }
-      };
    }
 
    function makeTerm(term, matchWholeWords) {
